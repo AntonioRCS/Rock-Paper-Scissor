c3960a0c33cb9feb5af7a3a6063bd6ae8c6ca5b9
AntonioRCS <atheglas@gmail.com>
2021-08-11 10:45:18 -0300

    Last Alt

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,15 @@
+const paperBtn = document.getElementById('paper-btn');
+const rockBtn = document.getElementById('rock-btn');
+const scissorBtn = document.getElementById('scissor-btn');
+const resetBtn = document.getElementById('reset-btn');
+
+const menu = document.getElementById("computer-menu");
+const playerText = document.getElementById('player-text');
+const computerText = document.getElementById('computer-text');
+
+const finalResultSection = document.getElementById("final-result");
+const winnerText = document.getElementById('winner-text');
+
 let finalResult;
 
 const winner = (playerChoice, computerChoice) => {
